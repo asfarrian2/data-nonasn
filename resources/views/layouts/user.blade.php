3a5965087a474f45a5e52a291cddfd146675c5cf
unknown <sahrulasfarianoor2@gmail.com>
2025-01-26 06:23:00 +0800

    pertama

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -81,7 +81,7 @@
               <ul class=" navbar-right">
                 <li class="nav-item dropdown open" style="padding-left: 15px;">
                   <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                  <img src="/logo/user.png" alt="">{{ Auth::guard('pegawai')->user()->nama }}
+                  <img src="/logo/User.png" alt="">{{ Auth::guard('pegawai')->user()->nama }}
                   </a>
                   <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item"  href="/profile/{{ Auth::guard('pegawai')->user()->id_nonasn }}"> Profil</a>
