067eaeeca8b86a92abed2651701499562edad0de
unknown <sahrulasfarianoor2@gmail.com>
2025-01-26 06:48:28 +0800

    pertama

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -53,15 +53,7 @@
         <br>
         <div class="col-md-4">
             <a href="/sk/create" class="btn btn-primary btn-sm" id="btntambahkoderekening">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus"
-                  width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                  stroke="currentColor" fill="none" stroke-linecap="round"
-                  stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M12 5l0 14"></path>
-                  <path d="M5 12l14 0"></path>
-              </svg>
-              Tambah
+              <i class="fa fa-plus"></i>Tambah
             </a>
         </div>
 
